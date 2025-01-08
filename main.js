6b478cdec02777e6b8c16b57f0d8ba0833b845de
Anurag Joshi <anuragj990@gmail.com>
2025-01-08 16:14:05 +0530

    1st try

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const path = require('path');
 // TODO: Use path.join to join two file path segments
 
 // TODO: Print the resulting path to the console
+
+
+const args=process.argv.slice(2);
+
+console.log(path.join(args[0],args[1]));
